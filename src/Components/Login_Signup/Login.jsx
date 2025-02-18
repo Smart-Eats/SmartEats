b498837ce3a7fc3d316d5e95ac8c8323f8801273
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2025-02-19 00:11:07 +0530

    Saving local changes before pulling

--- src/Components/Login_Signup/Login.jsx
+++ src/Components/Login_Signup/Login.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import "./Login.css";
-const Login = ({toggleForm}) => {
+import styles from "./login.module.css";
+
+const Login = ({ toggleForm }) => {
   return (
-    <div className="col align-items-center flex-col sign-up">
-      <div className="form-wrapper align-items-center">
-        <div className="form sign-up">
-          <div className="input-group">
+    <div className={`${styles.col} ${styles.alignItemsCenter} ${styles.flexCol} ${styles.signUp}`}>
+      <div className={`${styles.formWrapper} ${styles.alignItemsCenter}`}>
+        <div className={`${styles.form} ${styles.signUp}`}>
+          <div className={styles.inputGroup}>
             <i className="bx bxs-user"></i>
             <input type="text" placeholder="Username" />
           </div>
-          <div className="input-group">
+          <div className={styles.inputGroup}>
             <i className="bx bx-mail-send"></i>
             <input type="email" placeholder="Email" />
           </div>
-          <div className="input-group">
+          <div className={styles.inputGroup}>
             <i className="bx bxs-lock-alt"></i>
             <input type="password" placeholder="Password" />
           </div>
-          <div className="input-group">
+          <div className={styles.inputGroup}>
             <i className="bx bxs-lock-alt"></i>
             <input type="password" placeholder="Confirm password" />
           </div>
           <button>Sign up</button>
           <p>
             <span>Already have an account?</span>
-            <b onClick={toggleForm} className="pointer">
+            <b onClick={toggleForm} className={styles.pointer}>
               Sign in here
             </b>
           </p>
